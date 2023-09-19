06996c321e90f5434f453bd9a45f94527fa9577a
tgbhy <81264732+tgbhy@users.noreply.github.com>
2023-09-19 16:55:24 +0200

    Create tictactoe.py

---
+++ tictactoe.py
@@ -0,0 +1,22 @@
+def main():
+    print("Bienvenue dans le jeu du morpion !")
+    print("Démarrage du jeu en cours...")
+
+    print("Le joueur 1 est représenté par X et le joueur 2 est représenté par O !")
+    
+    print(" |  | ")
+    print("-|--|-")
+    print(" |  | ")
+    print("-|--|-")
+    print(" |  | ")
+
+    start_game()
+
+def start_game():
+    finish: bool = False
+
+    while finish == False:
+        print("test")
+        finish = True
+
+main()
